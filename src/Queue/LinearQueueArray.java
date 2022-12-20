b9ce12af0dd401b1eee31e58fdaaeb7dde90d618
Sangeet Redhu <sangeetredhu16@gmail.com>
2022-12-20 21:16:28 +0530

    adding Queue

---
+++ src/Queue/LinearQueueArray.java
@@ -0,0 +1,54 @@
+package Queue;
+
+
+public class LinearQueueArray {
+    static int front ;
+    static int rear ;
+    static int size ;
+    static int[] queue = new int[5];
+
+    static public void printQueue(){
+        for(int i=front;i<rear;i++){
+            System.out.print(queue[i]+" ");
+        }
+    }
+
+    public static void enqueue(int element){
+        if(rear==queue.length){
+            System.out.println("Queue is full .Element is not inserted : "+element);
+            return;
+        }
+
+        queue[rear] = element;
+        size++;
+        rear++;
+    }
+    public static void dequeue(){
+        if(front==rear){
+            System.out.println("Queue is empty");
+            return;
+        }
+        size--;
+        front++;
+
+    }
+
+    public static void main(String[] args) {
+        enqueue(10);
+        enqueue(20);
+        enqueue(30);
+        enqueue(40);
+        enqueue(40);
+        dequeue();
+        dequeue();
+        dequeue();
+        dequeue();
+        dequeue();
+        enqueue(80);
+        enqueue(90);
+
+
+        printQueue();
+    }
+}
+
